73285899aa1a5866fb0c980507c908b924037165
ygsamueljeong <46389189+ygsamueljeong@users.noreply.github.com>
2021-05-13 12:57:11 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
+const st = require('stocktwits');
+const axios  = require("axios");
 const express = require("express");
 const app = express();
 
 const port = 3001;
 const hostname = "localhost";
+app.use(express.json());
+
+
 
 app.get("/test", (req, res) => {
   console.log("hit test");
-  res.send("hello!");
+  st.get('streams/user/StockTwits', {limit:100}, function (err, res) {
+    // We parse the JSON to only handpick messages with words only have ing "$" and ".X" which signifies crypto
+    console.log(res.body);
+  })
+  res.send("API WORKS!");
 });
+ 
 
 app.listen(port, hostname, () => {
   console.log(`Listening at: http://${hostname}:${port}`);
