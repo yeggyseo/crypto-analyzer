e32270c132668fd7cdced0e08e6448ddbe8db885
ygsamueljeong <46389189+ygsamueljeong@users.noreply.github.com>
2021-05-14 03:13:41 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const st = require('stocktwits');
 const axios  = require("axios");
 const express = require("express");
 const app = express();
@@ -9,11 +8,57 @@ app.use(express.json());
 
 
 app.get("/test", (req, res) => {
-  console.log("hit test");
-  st.get('streams/user/StockTwits', {limit:100}, function (err, res) {
-    // We parse the JSON to only handpick messages with words only have ing "$" and ".X" which signifies crypto
-    console.log(res.body);
+  // API Call for trending symbols on StockTwits
+  axios.get('https://api.stocktwits.com/api/2/trending/symbols.json').then(res => {
+    let body = res.data.symbols;
+    let cryptos = []
+    for (let i=0; i < body.length; i++) {
+      let symbol = body[i].symbol;
+      // Handpicking crypto symbols (has ".X" at the end of the symbol name)
+      if (symbol.includes(".X")){
+        cryptos.push(symbol)
+      }
+    }
+    return cryptos;
+  }).then(res => {
+    let dict = {}; // Stores symbols as keys and tweets as values   { symbol: [message, message, ...] }
+    let cryptos = res;
+    // Async function to loop through API calls
+    async function getTweets() {
+      let response;
+      for (let i = 0; i < cryptos.length; i++) {
+        let symbol = cryptos[i]
+        response = await axios.get(`https://api.stocktwits.com/api/2/streams/symbol/${symbol}.json`).then(res => {
+          let messages = res.data.messages;
+          // Appending keys (symbols) and values (tweets) to the dictionary
+          for (let j = 0; j < messages.length; j++) {
+            if (dict.hasOwnProperty(symbol)) {
+              dict[symbol].push(messages[j].body);
+            } else {
+              dict[symbol]=[messages[j].body];
+            }  
+          }
+        })
+      }
+      return dict;
+    }
+    async function finalDict() {
+      return await getTweets();
+    }
+
+    return finalDict();
+
+  }).then(res => {
+    console.log(res);  
+    //
+    // IMPLEMENT SENTIMENTAL ANALYSIS
+    // res = { symbol: [message, message, ...] }
+    //
+    //
+    //
+    //
   })
+
   res.send("API WORKS!");
 });
  
